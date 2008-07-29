6ffbf956d7f140db856167e8df56f3e6fdaf99cd
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 20:19:30 +0000

    * Fixing whitespace issue with classifier text. * Fixing issues with parsing file docstring for descriptions.

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ class my_build(distbuild):
 setup(
     author = 'Enthought, Inc.',
     author_email = 'info@enthought.com',
-    classifiers = """\
+    classifiers = [c.strip() for c in """\
         Development Status :: 4 - Beta
         Intended Audience :: Developers
         Intended Audience :: Science/Research
@@ -147,7 +147,7 @@ setup(
         Topic :: Scientific/Engineering
         Topic :: Software Development
         Topic :: Software Development :: Libraries
-        """.splitlines(),
+        """.splitlines()],
     cmdclass = {
         'develop': my_develop,
         'build': my_build
@@ -155,13 +155,13 @@ setup(
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
         ],
-    description = DOCLINES[0],
+    description = DOCLINES[1],
     include_package_data = True,
     install_requires = [
         TRAITS,
         ],
     license = 'BSD',
-    long_description = '\n'.join(DOCLINES[2:]),
+    long_description = '\n'.join(DOCLINES[3:]),
     maintainer = 'ETS Developers',
     maintainer_email = 'enthought-dev@enthought.com',
     name = 'CodeTools',
