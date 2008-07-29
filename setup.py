17b30dc9687f0cdb95c04d1122e57ae6943fada0
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-29 23:46:30 +0000

    Added long description in docstring for display on PyPi.

--- setup.py
+++ setup.py
@@ -7,7 +7,16 @@
 """
 Code Analysis and Execution Tools
 
-<description text needed>
+The CodeTools project includes packages that simplify meta-programming and 
+help the programmer separate data from code in Python. This library contains 
+classes that allow defining simple snippets, or "blocks", of Python code, 
+analyze variable dependencies in the code block, and use these dependencies to 
+construct or restrict an execution graph. These (restricted) code blocks can
+then be executed in any namespace. However, this project also provides a
+Traits-event-enhanced namespace, called a "context", which can be used in 
+place of a vanilla namespace to allow actions to be performed whenever 
+variables are assigned or retrieved from the namespace. This project is used
+as the foundation for the BlockCanvas project.
 """
 
 
