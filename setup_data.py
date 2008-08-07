75ccbd90566e1121809bfeb51c679d1609217b75
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-07 20:27:26 +0000

    * Synchronized version numbers for ETS 3.0.0 release.

--- setup_data.py
+++ setup_data.py
@@ -13,7 +13,7 @@ def etsdep(p, min, max=None, literal=False):
     return require
 
 # Delcare our ETS project dependencies.
-TRAITS = etsdep('Traits', '3.0.1b1')
+TRAITS = etsdep('Traits', '3.0.1')
 
 
 # A dictionary of the setup data information.
