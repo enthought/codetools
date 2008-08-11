4d57673c523a510fc481aac3cb4be6bd3de8ede9
swisher <swisher@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-11 17:18:54 +0000

    Added the project to the packages' docstrings. This is obvious when looking at code, but not when looking at API docs.

--- enthought/execution/__init__.py
+++ enthought/execution/__init__.py
@@ -1,2 +1,10 @@
+#-----------------------------------------------------------------------------
+#
+#  Copyright (c) 2007 by Enthought, Inc.
+#  All rights reserved.
+#
+#-----------------------------------------------------------------------------
+""" Part of the CodeTools project of the Enthought Tool Suite.
+"""
 
 
