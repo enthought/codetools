4f9b7a6457e0b4b9421a9d7653eca072961ed51a
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-15 16:55:41 +0000

    * Updated to depend on the trunk version of Traits.

--- setup_data.py
+++ setup_data.py
@@ -13,7 +13,7 @@ def etsdep(p, min, max=None, literal=False):
     return require
 
 # Delcare our ETS project dependencies.
-TRAITS = etsdep('Traits', '3.0.2')
+TRAITS = etsdep('Traits', '3.0.3')
 
 
 # A dictionary of the setup data information.
