a2269a8f31677ae807e9f6a9233ffc899bd908ff
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 16:25:07 +0000

    * Removing dependency_links as we now publish releases to PyPi. * Code cleanup including update of copyright to include 2009.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2008 by Enthought, Inc.
+# Copyright (c) 2008-2009 by Enthought, Inc.
 # All rights reserved.
-#
+
 
 """
 Code Analysis and Execution Tools
@@ -26,12 +26,14 @@ If you want to build CodeTools from source, you must first install
 """
 
 
+import os
+import zipfile
+
 from distutils import log
 from distutils.command.build import build as distbuild
 from setuptools import setup, Extension, find_packages
 from setuptools.command.develop import develop
-import os
-import zipfile
+
 
 # FIXME: This works around a setuptools bug which gets setup_data.py metadata
 # from incorrect packages. Ticket #1592
@@ -39,6 +41,7 @@ import zipfile
 setup_data = dict(__name__='', __file__='setup_data.py')
 execfile('setup_data.py', setup_data)
 INFO = setup_data['INFO']
+
 
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
@@ -79,9 +82,6 @@ setup(
         'develop': MyDevelop,
         'build': MyBuild
     },
-    dependency_links = [
-        'http://code.enthought.com/enstaller/eggs/source',
-        ],
     description = DOCLINES[1],
     include_package_data = True,
     install_requires = INFO['install_requires'],
