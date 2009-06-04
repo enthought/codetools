59f93d18950b249ec690000dbc881fa7f1949f21
oliphant <oliphant@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-06-04 05:30:33 +0000

    Do some re-factoring of the func2str decorator to improve corner cases.  Needs more testing.

--- enthought/blocks/decorators.py
+++ enthought/blocks/decorators.py
@@ -7,65 +7,83 @@ import re
 
 from block import Block
 
-#single_quotes = re.compile("'.*'")
-#double_quotes = re.compile('".*"')
-#triple_quotes = re.compile('""".*"""')
-
-# Perhaps we can remove docstrings at some point, but
-#  probably not worth it.
-def strip_whitespace(source, name):
-    # Expand tabs to avoid any confusion.
-    wsource = [l.expandtabs(4) for l in source]
-
-    # Remove any code definition, and space lines
-    #  at the beginning
-    for i, line in enumerate(wsource):
-        if line.isspace():
-            continue
-        if line.startswith('def %s' % name):
-            continue
-        break
-    wsource = wsource[i:]
-
+def _detect_indentation_level(source):
     # Detect the indentation level
     done = False
-    for line in wsource:
+    for line in source:
         for col,char in enumerate(line):
             if char != ' ':
                 done = True
                 break
         if done:
             break
+    return col
+
+def _extract_indented_part(source, col):
     # Now we know how much leading space there is in the code.  Next, we
     # extract up to the first line that has less indentation.
-    # WARNINGS: we skip comments that may be misindented, but we do NOT yet
-    # detect triple quoted strings that may have flush left text.
-    for lno,line in enumerate(wsource):
-        lead = line[:col]
-        if lead.isspace():
+    #  We skip comments that may be misindented and detect triple quoted
+    #  strings and ignore indentation inside them.
+    triple_string = 0
+    src_lines = []
+    doc_lines = []
+    for lno,line in enumerate(source):
+        if triple_string:
+            if line.rstrip().endswith('"""'):
+                triple_string = 0
+            doc_lines.append(line)
             continue
+        lead = line[:col]
+        if lead.isspace():  # indented part
+            code = line[col:]
+            if code.startswith('"""'):
+                triple_string = 1
+                doc_lines.append(code)
+            else:
+                src_lines.append(code)
+            continue
+        if lead.lstrip().startswith('#'):  # comment line
+            src_lines.append(line.lstrip())
         else:
-            if not lead.lstrip().startswith('#'):
-                break
-    if (lno+1 == len(wsource)):
-        lno = lno + 1
-    # The real source is up to lno
-    src_lines = [l[col:] for l in wsource[:lno]]
+            break
     src = ''.join(src_lines)
+    return src, "".join(doc_lines)
+
+def _remove_head(source, name):
+    # Remove any code definition, and space lines
+    #  at the beginning
+    for i, line in enumerate(source):
+        if line.isspace():
+            continue
+        if line.startswith('def %s' % name):
+            continue
+        break
+    if len(source) > 0:
+        source = source[i:]
+    return source
+
+def strip_whitespace(source, name, spaces_for_tab):
+    # Expand tabs to avoid any confusion.
+    wsource = [l.expandtabs(spaces_for_tab) for l in source]
+
+    wsource = _remove_head(wsource, name)
+    col = _detect_indentation_level(wsource)
+    src, doc = _extract_indented_part(wsource, col)
+
     #print 'SRC:\n<<<<<<<>>>>>>>\n%s<<<<<>>>>>>' % src  # dbg
     return src
 
 def findsource_file(f, name):
     lines = linecache.getlines(f.f_code.co_filename)
     wsource = lines[f.f_lineno:]
-    return strip_whitespace(wsource, name)
+    return strip_whitespace(wsource, name, 8)
 
 def findsource_ipython(f, name):
     from IPython import ipapi
     ip = ipapi.get()
     wsource = [l+'\n' for l in
                ip.IP.input_hist_raw[-1].splitlines()[1:]]
-    return strip_whitespace(wsource, name)
+    return strip_whitespace(wsource, name, 4)
 
 
 def func2str(func,backframes=1):
