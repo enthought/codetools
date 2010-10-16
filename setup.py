e6a0e217aeff20443518636e67c8e0f2df72d108
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-10-16 04:52:30 +0000

    add download url (to make next release easier)

--- setup.py
+++ setup.py
@@ -70,6 +70,8 @@ class MyBuild(distbuild):
 setup(
     author = 'Enthought, Inc.',
     author_email = 'info@enthought.com',
+    download_url = ('http://www.enthought.com/repo/ETS/CodeTools-%s.tar.gz' %
+                    INFO['version']),
     classifiers = [c.strip() for c in """\
         Development Status :: 5 - Production/Stable
         Intended Audience :: Developers
