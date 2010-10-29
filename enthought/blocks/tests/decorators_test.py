d5783418e911214ea386de9e4e3c4364ecd0f352
jmarch <jmarch@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-10-29 21:20:00 +0000

    TST: Codetools: block decorators: test func2str

---
+++ enthought/blocks/tests/decorators_test.py
@@ -0,0 +1,44 @@
+from numpy.testing import assert_equal
+
+from enthought.blocks.decorators import func2str
+
+def test_func2str():
+    expected_lines = "a=1\nb=2\n"
+
+    "dummy string"
+    @func2str
+    # dummy comment
+    def f1():
+        a=1
+        b=2
+    dummy = 3
+    assert_equal(f1, expected_lines)
+
+    @func2str
+    # dummy comment
+    def f1(x):
+        a=1
+        b=2
+    assert_equal(f1, expected_lines)
+
+    @func2str
+    # dummy comment
+    def f1(x=1):
+        a=1
+        b=2
+    assert_equal(f1, expected_lines)
+
+    @func2str
+    def f1(x=1):
+        a=1
+        b=2
+    assert_equal(f1, expected_lines)
+
+    @func2str
+    def f1():
+        def f1sub():
+            pass
+        a=1
+        b=2
+    assert_equal(f1, 'def f1sub():\n    pass\n' + expected_lines)
+
