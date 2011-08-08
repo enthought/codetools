2bc17922397bd1dc138bbdd7981bfbb49f50c2b7
srossross <srossross@enthought.com>
2011-08-08 15:53:26 -0500

    Deleted blocks 2

--- codetools/blocks2/__init__.py
+++
@@ -1,8 +0,0 @@
-#-----------------------------------------------------------------------------
-#
-#  Copyright (c) 2007 by Enthought, Inc.
-#  All rights reserved.
-#
-#-----------------------------------------------------------------------------
-""" Part of the CodeTools project of the Enthought Tool Suite.
-"""
