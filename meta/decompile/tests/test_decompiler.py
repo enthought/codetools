02e0dc4e30a5f377354cb6f17fd0660e8ba10327
srossross <srossross@enthought.com>
2011-11-06 16:26:00 -0600

    Added tests for failing list comprehensions.

--- meta/decompile/tests/test_decompiler.py
+++ meta/decompile/tests/test_decompiler.py
@@ -9,6 +9,7 @@ import _ast
 from meta.decompile import make_module
 from meta.asttools import cmp_ast, print_ast
 from meta.testing import py2, py2only
+from meta.decompile.tests import Base
 
 
 if py2:
@@ -17,54 +18,6 @@ else:
     from io import StringIO
     
 filename = 'tests.py'
-
-
-py2only
-
-class Base(unittest.TestCase):
-
-    def assertAstEqual(self, left, right):
-
-        if not isinstance(left, _ast.AST):
-            raise self.failureException("%s is not an _ast.AST instance" % (left))
-        if not isinstance(right, _ast.AST):
-            raise self.failureException("%s is not an _ast.AST instance" % (right))
-        result = cmp_ast(left, right)
-
-        if not result:
-            
-            lstream = StringIO()
-            print_ast(left, indent='', file=lstream, newline='')
-
-            rstream = StringIO()
-            print_ast(right, indent='', file=rstream, newline='')
-
-            lstream.seek(0)
-            rstream.seek(0)
-            msg = 'Ast Not Equal:\nGenerated: %r\nExpected:  %r' % (lstream.read(), rstream.read())
-            raise self.failureException(msg)
-
-
-
-    def statement(self, stmnt, equiv=None):
-
-        if equiv is None:
-            expected_ast = compile(stmnt, filename, 'exec', _ast.PyCF_ONLY_AST)
-            code = compile(expected_ast, filename, 'exec')
-            mod_ast = make_module(code)
-        else:
-            expected_ast_ = compile(stmnt, filename, 'exec', _ast.PyCF_ONLY_AST)
-            code = compile(expected_ast_, filename, 'exec')
-            mod_ast = make_module(code)
-
-            expected_ast = compile(equiv, filename, 'exec', _ast.PyCF_ONLY_AST)
-
-        self.assertAstEqual(mod_ast, expected_ast)
-
-        code = compile(mod_ast, filename, 'exec')
-        expected_code = compile(expected_ast, filename, 'exec')
-
-        self.assertEqual(code.co_code, expected_code.co_code)
 
 
 class Simple(Base):
@@ -399,6 +352,7 @@ class Simple(Base):
         self.statement(stmnt)
 
 class LogicJumps(Base):
+        
     def test_logic1(self):
         'a and b or c'
         self.statement('a and b or c')
@@ -407,29 +361,6 @@ class LogicJumps(Base):
         'a or (b or c)'
         self.statement('a or (b or c)')
 
-    def test_list_comp1(self):
-        stmnt = '[a for b in c]'
-        self.statement(stmnt)
-
-    def test_list_comp2(self):
-        stmnt = '[a() +1 for b in c]'
-        self.statement(stmnt)
-
-    def test_list_comp3(self):
-        stmnt = 'y = [a() +1 for b in c]'
-        self.statement(stmnt)
-
-    def test_list_comp_ifs(self):
-        stmnt = 'y = [a() +1 for b in c if asdf]'
-        self.statement(stmnt)
-
-    def test_list_comp_ifs1(self):
-        stmnt = 'y = [a() +1 for b in c if asdf if asd]'
-        self.statement(stmnt)
-
-    def test_list_comp_ifs2(self):
-        stmnt = 'y = [a() +1 for b in c if asdf if not asd]'
-        self.statement(stmnt)
 
     def test_if_expr_discard(self):
 
